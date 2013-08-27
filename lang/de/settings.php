9a25364265018b307cb551c94ed6e1eddcd3b305
Merlin Nagel <merlin.nagel@gmail.com>
2013-08-28 00:15:33 +0200

    translation update

---
+++ lang/de/settings.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Merlin Nagel <merlin.nagel@gmail.com>
+ */
+$lang['maxlen']                = 'Zu verwendende maximale Anzahl Zeichen für den angezeigten Überschriften. Keine Kürzung beim Wert 0.';
+$lang['imgw']                  = 'Bild Breite';
+$lang['imgh']                  = 'Bild Höhe';
+$lang['showprice']             = 'Den niedrigsten Amazon Preis anzeigen.';
+$lang['partner_us']            = 'PartnerID für den amerikanischen Amazon Laden.';
+$lang['partner_de']            = 'PartnerID für den deutschen Amazon Laden.';
+$lang['partner_jp']            = 'PartnerID für den japanischen Amazon Laden.';
+$lang['partner_uk']            = 'PartnerID für den britischen Amazon Laden.';
+$lang['partner_fr']            = 'PartnerID für den französischen Amazon Laden.';
+$lang['partner_ca']            = 'PartnerID für den kanadischen Amazon Laden.';
+$lang['showpurchased']         = 'Bereits gekaufte Artikel in die angezeigte Listen einfügen.';
+$lang['sort']                  = 'Anzuwendende Sortierkriterien für die angezeigten Listen.';
