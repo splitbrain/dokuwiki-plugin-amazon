85f9d65950ab075cae3f642586f23bb2379d75f7
Fabrice Dejaigher <fabrice@chtiland.com>
2014-01-03 12:00:04 +0100

    translation update

---
+++ lang/fr/settings.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Fabrice Dejaigher <fabrice@chtiland.com>
+ */
+$lang['publickey']             = 'Votre AWS Access Key ID. (Peut être trouvée <a href="https://aws-portal.amazon.com/gp/aws/developer/account/index.html?ie=UTF8&action=access-key">ici</a>)';
+$lang['privatekey']            = 'Votre AWS Secret Access Key. (Peut être trouvée <a href="https://aws-portal.amazon.com/gp/aws/developer/account/index.html?ie=UTF8&action=access-key">ici</a>)';
+$lang['maxlen']                = 'Nombre maximum de caractères à utiliser pour afficher le titre. Mettez 0 pour illimité.';
+$lang['imgw']                  = 'Largeur de l\'image';
+$lang['imgh']                  = 'Hauteur de l\'image';
+$lang['showprice']             = 'Afficher les prix les plus bas d\'Amazon.';
+$lang['partner_us']            = 'PartnerID pour le magasin Amazon Amérique.';
+$lang['partner_de']            = 'PartnerID pour le magasin Amazon Allemagne.';
+$lang['partner_jp']            = 'PartnerID pour le magasin Amazon Japon.';
+$lang['partner_uk']            = 'PartnerID pour le magasin Amazon britanique.';
+$lang['partner_fr']            = 'PartnerID pour le magasin Amazon France.';
+$lang['partner_ca']            = 'PartnerID pour le magasin Amazon Canada.';
+$lang['showpurchased']         = 'Inclure les éléments déjà acheté dans l\'affichage des listes.';
+$lang['sort']                  = 'Critère de tri à utiliser pour l\'affichage des listes.';
