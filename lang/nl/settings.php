a2a439d60e109ab0011ad2c7592e4cf3b7827f31
Rene <wllywlnt@yahoo.com>
2014-03-20 20:15:20 +0100

    translation update

---
+++ lang/nl/settings.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Rene <wllywlnt@yahoo.com>
+ */
+$lang['publickey']             = 'Je AWS Access Key ID. (Kan worden gevonden <a href="https://aws-portal.amazon.com/gp/aws/developer/account/index.html?ie=UTF8&action=access-key">hier</a>)\';
+';
+$lang['privatekey']            = 'Je AWS Secret Access Key. (Kan worden gevonden <a href="https://aws-portal.amazon.com/gp/aws/developer/account/index.html?ie=UTF8&action=access-key">hier</a>)\';
+';
+$lang['maxlen']                = 'Maximaal aantal karakters dat wordt weergegeven voor de getoonde titels. Stel in op 0 indien er niet moet worden ingekort.';
+$lang['imgw']                  = 'Afbeelding breedte';
+$lang['imgh']                  = 'Afbeelding hoogte';
+$lang['showprice']             = 'Kies de laagste Amazon prijs';
+$lang['partner_us']            = 'PartnerID voor de Amerikaanse Amazon winkel.';
+$lang['partner_de']            = 'PartnerID voor de Amerikaanse Amazon winkel.';
+$lang['partner_jp']            = 'PartnerID voor de Japanse Amazon winkel.';
+$lang['partner_uk']            = 'PartnerID voor de Britse Amazon winkel.';
+$lang['partner_fr']            = 'PartnerID voor de Franse Amazon winkel.';
+$lang['partner_ca']            = 'PartnerID voor de Canadese Amazon winkel.';
+$lang['showpurchased']         = 'Vermeld reeds aangekochte voorwerpen in de weergegeven lijst.';
+$lang['sort']                  = 'Sorteer criteria om in de weergegeven lijst te gebruiken.';
