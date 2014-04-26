0029c5b25789afb9a4374bfa7c5868d13a934d08
Marina Vladi <deldadam@gmail.com>
2014-04-26 21:20:04 +0200

    translation update

--- lang/hu/settings.php
+++ lang/hu/settings.php
@@ -4,10 +4,19 @@
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * 
  * @author Serenity87HUN <anikototh87@gmail.com>
+ * @author Marina Vladi <deldadam@gmail.com>
  */
-$lang['maxlen']                = 'A címben szereplő karakterek maximális száma. Állítsd 0-ra, ha nem szeretnél rövidített címeket.';
+$lang['publickey']             = 'AWS hozzáférési kulcsazonosítónk (ID). (Megtalálható <a href="https://aws-portal.amazon.com/gp/aws/developer/account/index.html?ie=UTF8&action=access-key">itt</a>.)';
+$lang['privatekey']            = 'AWS titkos hozzáférési kulcsunk. (Megtalálható <a href="https://aws-portal.amazon.com/gp/aws/developer/account/index.html?ie=UTF8&action=access-key">itt</a>.)';
+$lang['maxlen']                = 'A címben szereplő karakterek maximális száma. Állítsuk 0-ra, ha nem szeretnénk rövidített címeket.';
 $lang['imgw']                  = 'Képszélesség';
 $lang['imgh']                  = 'Képmagasság';
-$lang['showprice']             = 'Legalacsonyabb Amazon ár mutatása.';
-$lang['showpurchased']         = 'Már megvásárolt termékek mutatása a listában.';
-$lang['sort']                  = 'Megjelenített lista rendezési feltétele.';
+$lang['showprice']             = 'Legalacsonyabb Amazon-ár mutatása.';
+$lang['partner_us']            = 'Partnerazonosító (ID) az amerikai Amazon-áruházhoz.';
+$lang['partner_de']            = 'Partnerazonosító (ID) a német Amazon-áruházhoz.';
+$lang['partner_jp']            = 'Partnerazonosító (ID) a japán Amazon-áruházhoz.';
+$lang['partner_uk']            = 'Partnerazonosító (ID) a brit Amazon-áruházhoz.';
+$lang['partner_fr']            = 'Partnerazonosító (ID) a francia Amazon-áruházhoz.';
+$lang['partner_ca']            = 'Partnerazonosító (ID) a kanadai Amazon-áruházhoz.';
+$lang['showpurchased']         = 'Már megvásárolt termékek megjelenítése a listában.';
+$lang['sort']                  = 'Rendezési feltétel lista megjelenítéséhez.';
