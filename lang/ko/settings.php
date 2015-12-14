ea22505c0c4c4e99380f32f4e9860a6ac76193f5
Erial <erial2@gmail.com>
2015-12-14 03:45:32 +0100

    translation update

---
+++ lang/ko/settings.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Erial <erial2@gmail.com>
+ */
+$lang['publickey']             = 'AWS 억세스 키 ID (<a href="https://aws-portal.amazon.com/gp/aws/developer/account/index.html?ie=UTF8&action=access-key">여기</a>에서 찾을 수 있습니다)';
+$lang['privatekey']            = 'AWS 암호화 억세스 키 (<a href="https://aws-portal.amazon.com/gp/aws/developer/account/index.html?ie=UTF8&action=access-key">여기</a>에서 찾을 수 있습니다)';
+$lang['maxlen']                = '제목 표시에 사용할 최대 문자 수. 0으로 설정하면 제한하지 않습니다.';
+$lang['imgw']                  = '이미지 너비';
+$lang['imgh']                  = '이미지 높이';
+$lang['showprice']             = '가장 저렴한 아마존 가격을 보여줍니다.';
+$lang['partner_us']            = '아마존 미국 파트너 ID ';
+$lang['partner_de']            = '아마존 독일 파트너 ID ';
+$lang['partner_jp']            = '아마존 일본 파트너 ID ';
+$lang['partner_uk']            = '아마존 영국 파트너 ID ';
+$lang['partner_fr']            = '아마존 프랑스 파트너 ID ';
+$lang['partner_ca']            = '아마존 캐나다 파트너 ID ';
+$lang['showpurchased']         = '이미 구매한 항목도 표시.';
+$lang['sort']                  = '목록 정렬에 사용할 기준.';
