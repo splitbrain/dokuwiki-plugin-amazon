1e0a2ca0de0707fc40aa476c4a36de583f23484c
2576562185 <2576562185@qq.com>
2017-08-27 11:25:45 +0200

    translation update

---
+++ lang/zh/settings.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author 2576562185 <2576562185@qq.com>
+ */
+$lang['imgw']                  = '图片宽度';
+$lang['imgh']                  = '图片高度';
+$lang['showprice']             = '显示亚马逊最低价格';
